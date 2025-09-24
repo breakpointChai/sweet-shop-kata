b869e8d2ab362b70b7bf3910d3be8be685f24fbe
brakpointChai <solankipradeep034@gmail.com>
2025-09-24 22:34:56 +0530

    SETUP PROJECT STRUCTURE AND ADD DEPENDENCIES

---
+++ src/main/java/com/ai/sweet_shop_kata/SweetShopKataApplication.java
@@ -0,0 +1,13 @@
+package com.ai.sweet_shop_kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetShopKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetShopKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai/sweet_shop_kata/SweetShopKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.sweet_shop_kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetShopKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
