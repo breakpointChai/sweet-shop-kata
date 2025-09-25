aa485c048d3a049610515e55fbe507203ad21890
brakpointChai <solankipradeep034@gmail.com>
2025-09-25 15:34:10 +0530

    Add PUT and DELETE endpoints for updates and removals

--- src/main/java/com/ai/sweet_shop_kata/controller/SweetController.java
+++ src/main/java/com/ai/sweet_shop_kata/controller/SweetController.java
@@ -5,10 +5,7 @@ import com.ai.sweet_shop_kata.service.SweetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/sweets")
@@ -21,6 +18,23 @@ public class SweetController {
         SweetDto createdSweet=sweetService.addSweet(sweetDto);
         return new ResponseEntity<>(createdSweet, HttpStatus.CREATED);
     }
+
+    // update sweet
+    @PutMapping("/{id}")
+    public ResponseEntity<SweetDto> updateSweet(@PathVariable String id, @RequestBody SweetDto sweetDto) {
+        sweetDto.setId(id);
+        SweetDto updatedSweet = sweetService.updateSweet(sweetDto);
+        return new ResponseEntity<>(updatedSweet, HttpStatus.OK);
+    }
+
+    // delete sweet
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSweet(@PathVariable String id) {
+        sweetService.deleteSweet(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
 
 
 }
