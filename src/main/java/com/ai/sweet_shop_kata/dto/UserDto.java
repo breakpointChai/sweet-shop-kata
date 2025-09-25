798ed395628dbc38c413e16116a658f2580c02d3
brakpointChai <solankipradeep034@gmail.com>
2025-09-25 17:46:05 +0530

    Implement User Dto

---
+++ src/main/java/com/ai/sweet_shop_kata/dto/UserDto.java
@@ -0,0 +1,28 @@
+package com.ai.sweet_shop_kata.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    private String userId;
+
+    private String name;
+
+
+    private String email;
+
+    private String password;
+
+    private String gender;
+
+    private String about;
+    
+    private String imageName;
+}
